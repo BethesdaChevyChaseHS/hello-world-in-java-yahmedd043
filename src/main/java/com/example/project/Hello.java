f8a53b71837655f2b748d52254c41577389f5d7d
yahmedd043 <yahmedd043@gmail.com>
2025-08-28 13:41:42 +0000

    hello world

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(final String[] args) {
 		//Change the text to say, "Hello world!"
-		System.out.println("Hi planet");
+		System.out.println("Hello world!");
 	}
 
 }
